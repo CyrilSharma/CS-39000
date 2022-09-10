76ada831b4363844a627c5fdd3d277cdbe4eb112
CyrilSharma <sharma.cyril@gmail.com>
2022-09-10 12:50:59 -0400

    anotha few

---
+++ week3/basket/basket.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+int n;
+
+int dfs(int depth, int d, int prev, int weight, vector<ll> fruits) {
+    int total = 0;
+    if (weight >= 200) return 0;
+    if (depth == d) return weight;
+    // start at prev+1 to ensure you choose a unique fruit.
+    for (int i = prev+1; i < n; i++) {
+        weight += fruits[i];
+        total += dfs(depth, d+1, i, weight, fruits);
+        weight -= fruits[i];
+    }
+    return total;
+}
+
+int main() {
+    cin >> n;
+    vector<ll> fruits(n);
+    // each item can either be included or excluded -> 2^n total baskets.
+    ll nbask = (ll) pow((double)2, (double)(n));
+    ll tweight = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> fruits[i];
+        // in half of the total baskets an item is included, thus 1/2 * nback * weight 
+        // summed for all the fruits is the total weight of all baskets.
+        tweight += nbask/2 * fruits[i];
+    }
+
+    ll sweight = 0;
+    for (int i = 1; i <= 4; i++) {
+        // compute the weight of baskets with weight < 200 using 1,2,3 and 4 fruit.
+        sweight += dfs(i, 0, -1, 0, fruits);
+    }
+    // the answer is the total number of baskets minus the number of invalid baskets.
+    cout << tweight - sweight << "\n";
+}
--- week3/layout/layout.cpp
+++ week3/layout/layout.cpp
@@ -7,8 +7,57 @@
 
 using namespace std;
 
-#define maxVal 1e200
+struct story { int w,h,x,y; };
+typedef vector<story> vs;
+typedef vector<int> vi;
+
+bool overlaps(story a, story b) {
+    // right boundary of left object
+    int r = min(a.x + a.w, b.x + b.w);
+    // left boundary of right object
+    int l = max(a.x, b.x);
+    int t = min(a.y + a.h, b.y + b.h);
+    int bo = max(a.y, b.y);
+
+    // if right boundary of left object is past left boundary of right object
+    // then there is overlap.
+    if (r > l && t > bo) return true;
+    return false;
+}
+
+int n;
+int dfs(int prev, int area, vi exclude, vs stories) {
+    int narea = area;
+    // find the maximum area from the remaining possibilities.
+    // start at prev+1 so that you don't use identical combinations (a, b) vs (b, a)
+    for (int i = prev+1; i < n; i++) {
+        if (exclude[i]) continue;
+        // represents indexes that cannot be chosen
+        // if there is overlap, or if you have already chosen the index,
+        // mark the index as unuseable.
+        vi nexclude(n);
+        for (int j = 0; j < n; j++) {
+            nexclude[j] = exclude[j];
+            if (overlaps(stories[i], stories[j])) 
+                nexclude[j] = 1;
+        }
+        nexclude[i] = 1;
+        int sarea = stories[i].w * stories[i].h;
+        narea = max(narea, dfs(i, area + sarea, nexclude, stories));
+    }
+    return narea;
+}
 
 int main() {
-    ;
+    cin >> n;
+    while (n != 0) {
+        vi exclude(n); vs stories(n);
+        for (int i = 0; i < n; i++) {
+            story s;
+            cin >> s.w >> s.h >> s.x >> s.y;
+            stories[i] = s;
+        }
+        cout << dfs(-1, 0, exclude, stories) << "\n";
+        cin >> n;
+    }
 }
